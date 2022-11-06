2cecfa4c3805bbda5eedb2dc16f1c979b0ba7cd7
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-11-06 21:09:18 +0530

    Validate Last Name
    Validate Last Name

---
+++ LE2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace LE2_LastName
+{
+    public class LastName
+    {
+        //pattern needed for string
+        public string reg = "^[A-Z][A-za-z]{2,}";
+        //comparing both values using regex
+        public bool FirstNameValidate(string lastname) => Regex.IsMatch(lastname, reg) ? true : false;
+    }
+    internal class LE1
+    {
+        static void Main(string[] args)
+        {
+            LastName ln = new LastName();
+            //accepting user input
+            Console.Write("Enter The Last Name:");
+            string lname = Console.ReadLine();
+            bool result = ln.FirstNameValidate(lname);
+            if (result)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not Valid");
+            }
+        }
+    }
+}
