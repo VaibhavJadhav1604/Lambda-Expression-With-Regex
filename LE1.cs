2b25a3b22e1bce1a5b6a03a6239c94e5a5a53fe2
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-11-06 21:06:22 +0530

    Validate First Name
    Validate First Name

---
+++ LE1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace LE1_FirstName
+{
+    public class FirstName
+    {
+        //pattern needed for string
+        public string reg = "^[A-Z][A-za-z]{2,}";
+        //comparing both values using regex
+        public bool FirstNameValidate(string firstname) => Regex.IsMatch(firstname,reg) ? true: false;
+    }
+    internal class LE1
+    {
+        static void Main(string[] args)
+        {
+            FirstName fn=new FirstName();
+            //accepting user input
+            Console.Write("Enter The First Name:");
+            string fname=Console.ReadLine();
+            bool result = fn.FirstNameValidate(fname);
+            if(result)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not Valid");
+            }
+        }
+    }
+}
